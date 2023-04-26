c072f45f240cd4ddbcbbacbf1d053842a3ff582f
Katarzyna Dobry <katarzynakietrys@wp.pl>
2023-04-26 11:06:52 +0200

    xxx

--- Kodilla-intro/src/main/java/HelloWorld.java
+++ Kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Katarzyna");
+        System.out.println("30");
+        System.out.println("D");
+    }
 }
---
+++ Kodilla-intro/src/main/java/com/kodilla/Kasia.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+public class Kasia {
+    public static void main(String[] args) {
+
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
