f9427f3acec9bf7ca8f80f47f3e82865bb34d964
Katarzyna Dobry <katarzynakietrys@wp.pl>
2023-05-22 19:25:53 +0200

    FirstClass

--- Kodilla-intro/src/main/java/com/kodilla/FirstClass.java
+++ Kodilla-intro/src/main/java/com/kodilla/FirstClass.java
@@ -4,13 +4,12 @@ import com.kodilla.Notebook;
 
 public class FirstClass
 {public static void main(String[] args) {
-    Notebook notebook = new Notebook(600,1000);
+    Notebook notebook = new Notebook(600, 1000);
     System.out.println(notebook.weight + " " + notebook.price);
-    Notebook heavyNotebook = new Notebook (2000, 1500);
-    System.out.println("heavyNotebook.weight:" + heavyNotebook.weight);
-    System.out.println("heavyNotebook.price:" + heavyNotebook.price);
-    Notebook oldNotebook = new Notebook (1200,500);
-    System.out.println("oldNotebook.weight:" + oldNotebook.weight);
-    System.out.println("oldNotebook.price:" + oldNotebook.price);
-}
+    if (notebook.price < 900) {
+    System.out.println("This notebook is cheap.");
+    } else {
+        System.out.println("This notebook is quite expensive.");
+    }
+    }
 }
