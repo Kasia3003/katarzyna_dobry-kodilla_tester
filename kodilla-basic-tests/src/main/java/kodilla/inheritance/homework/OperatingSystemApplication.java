5541f7aecd189707494cf00b55af889f680c5a9e
Katarzyna Dobry <katarzynakietrys@wp.pl>
2023-05-30 16:13:51 +0200

    OperatingSystem

--- kodilla-basic-tests/src/main/java/kodilla/inheritance/homework/OperatingSystemApplication.java
+++ kodilla-basic-tests/src/main/java/kodilla/inheritance/homework/OperatingSystemApplication.java
@@ -7,7 +7,12 @@ public class OperatingSystemApplication {
         operatingSystem.turnOff();
         System.out.println(operatingSystem.getYear());
         OperatingSystemAA operatingSystemAA = new OperatingSystemAA(2021);
+        operatingSystemAA.turnOn();
         operatingSystemAA.turnOff();
         System.out.println(operatingSystemAA.getYear());
+        OperatingSystemBB operatingSystemBB = new OperatingSystemBB(2022);
+        operatingSystemBB.turnOn();
+        operatingSystemBB.turnOff();
+        System.out.println(operatingSystemBB.getYear());
     }
 }
--- kodilla-basic-tests/src/main/java/kodilla/inheritance/homework/OperatingSystemBB.java
+++ kodilla-basic-tests/src/main/java/kodilla/inheritance/homework/OperatingSystemBB.java
@@ -1,15 +1,19 @@
-//package kodilla.inheritance.homework;
-//
-//public class OperatingSystemBB extends OperatingSystem {
-//    public OperatingSystemBB(int year) {
-//
-//        super(year);
-//    }
-//    @Override
-//    public void TurnOn() {
-//    public OperatingSystemBB(int year) {
-//        super(year);
-//        System.out.println("Operating System turned on 2022");
-//        }
-//    }
-//}
+package kodilla.inheritance.homework;
+
+public class OperatingSystemBB extends OperatingSystem {
+
+
+    public OperatingSystemBB(int year) {
+        super(year);
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Bla, bla");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Operating System turned on 2022");
+    }
+}
