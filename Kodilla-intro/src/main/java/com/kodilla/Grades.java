10530ce526dd4b59e935d7852eaecf9da16836ca
Katarzyna Dobry <katarzynakietrys@wp.pl>
2023-05-30 16:38:37 +0200

    Grades

--- Kodilla-intro/src/main/java/com/kodilla/Grades.java
+++ Kodilla-intro/src/main/java/com/kodilla/Grades.java
@@ -1,21 +1,21 @@
 package com.kodilla;
 
 public class Grades {
+    public static int lenght;
     private int[] grades;
     private int size;
 
     public Grades() {
         this.grades = new int[10];
         this.size = 0;
+        this.size = 1;
     }
 
     public void add(int value) {
 
-
         this.grades[this.size] = value;
         this.size++;
-
     }
-}
+    }
 
 
--- Kodilla-intro/src/main/java/com/kodilla/GradesMain.java
+++ Kodilla-intro/src/main/java/com/kodilla/GradesMain.java
@@ -4,6 +4,6 @@ public class GradesMain {
     public static void main(String[] args) {
         Grades grades = new Grades();
         grades.add(1);
-
+        for (int i = 0; i < Grades.lenght; i++);
     }
 }
